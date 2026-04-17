6b6bf08a7c86beb15c90918ac950c055f36f7342
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-04-17 14:12:41 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -46,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Hello world usign the flutter'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
